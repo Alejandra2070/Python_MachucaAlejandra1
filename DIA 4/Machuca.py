9da3cd4a73c23d0f078f1a3b0c848e522bce170f
AlejaMachuca <am3793825@gmail.com>
2024-04-12 16:53:35 -0500

    holi

--- DIA 4/Machuca.py
+++ DIA 4/Machuca.py
@@ -1,30 +1,29 @@
-#-------------------------------
-#------------DIA 4--------------
-#-------------------------------
+#------------------------------
+#-----------DIA 4--------------
+#------------------------------
 
 #imprimir bienvenidos
-print("BIENVENIDOSSS!!! :D")
-print("En este programa vamos a calcular el mínimo de monedas  necesarias para cambiar el valor dado")
-import string
+print("BIENVENIDOSSS!!")
+print("En este programa vamos a calcular cuántas monedas se necesitan para un cambio")
 
-dinero = int(input("Ingrese la cantidad que desea cambiar: "))
+def cambio_money(cant):
+    monedas = [10, 5, 1]
+    number_money = [0, 0, 0]
+    for i, moneda in enumerate(monedas):
+        number_money[i] = cant // moneda
+        cant %= moneda
 
-lista = [1, 5, 10]
+    return number_money
 
-for i in lista:
- 
-    if dinero <= i:
-     queda=dinero//i
-    print("Existen " + string(queda) + ("billetes" if dinero <=1 else "monedas") + " de " + string (i) + "pesos" )
-    dinero = dinero % i
-
-    if dinero <= i:
-     queda=dinero//i
-    print("Existen " + string(queda) +  "billetes de 5")
-    dinero = dinero % i
-
-    if dinero <= i:
-     queda=dinero//i
-    print("Existen " + string(queda) +  "billetes de 10")
-    dinero = dinero % i
-    
+cant = int(input("Ingresa la cantidad que deseas cambiar: "))
+monedas = cambio_money(cant)
+print("")
+print(f"Para cambiar {cant} se van a necesitar: ")
+print("")
+print(f"{monedas[2]} monedas de 1")
+print(f"{monedas[1]} monedas de 5")
+print(f"{monedas[0]} monedas de 10")
+print("")
+print("Esperamos te haya gustado nuestro programa, te esperamos pronto!!")
+print("Que tengas un bonito día")
+print("BAYYYYY :D")
