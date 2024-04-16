95c809da4f7ec6dfe52f5b45c5a462779b9b4499
AlejaMachuca <am3793825@gmail.com>
2024-04-15 19:30:07 -0500

    holi

---
+++ DIA 5/Aleja.py
@@ -0,0 +1,19 @@
+#---------------------------
+#----------DIA 5------------
+#---------------------------
+
+cantidad = int(input())
+datos = []
+
+for i in range(cantidad):
+    dato = input()
+    datos.append(dato)
+
+datos_ordenados = sorted(datos)
+print(datos_ordenados)
+
+#en el primer espacio se escriben la cantidad de datos que va a ingresar
+#luego de eso damos un enter y empezamos a escribir nuestros datos siempre dando enter al finalizar cada uno
+#por último este nos mostrará el resultado en la lista
+
+#Desarrollado por Alejandra Machuca T.I 1093593725
