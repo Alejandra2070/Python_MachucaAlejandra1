f3852ca5e3eb8a316af602fb5f2018cec6d9552c
Alejandra2070 <am3793825@gmail.com>
2024-04-22 13:00:04 -0500

    holi

---
+++ DIA 11/Ejercicios.py
@@ -0,0 +1,16 @@
+#---------------------------------
+#-------------DIA 11--------------
+#---------------------------------
+
+lista = []
+lista = input()
+objetivo = []
+elemento = input()
+contador = 0
+for i in range(len(lista)):
+    contador+=1
+    if i == objetivo:
+     contador=contador-1
+print(contador)
+
+#Desarrollado por Alejandra Machuca Grupo T2
